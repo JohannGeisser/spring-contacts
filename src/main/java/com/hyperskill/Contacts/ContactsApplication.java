ff0e20f7736d07b584f0909d9e72a2dcbfe668b9
Johann <johann.geisser.aguilar@gmail.com>
2022-08-03 16:56:44 +0200

    Adding all project files

---
+++ src/main/java/com/hyperskill/Contacts/ContactsApplication.java
@@ -0,0 +1,13 @@
+package com.hyperskill.Contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyperskill/Contacts/ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperskill.Contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
