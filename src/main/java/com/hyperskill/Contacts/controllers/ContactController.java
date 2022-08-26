9c7da3ef1b9a9d0dee32ddeaaefc4da8bfd02d4e
Johann <johann.geisser.aguilar@gmail.com>
2022-08-26 17:11:46 -0400

    Adding dependency injection so that objects can be automatically instantiated

--- src/main/java/com/hyperskill/Contacts/controllers/ContactController.java
+++ src/main/java/com/hyperskill/Contacts/controllers/ContactController.java
@@ -2,6 +2,7 @@ package com.hyperskill.Contacts.controllers;
 
 import com.hyperskill.Contacts.models.Contact;
 import com.hyperskill.Contacts.services.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 public class ContactController {
     private final ContactService contactService;
 
+    @Autowired
     public ContactController(ContactService contactService) {
         this.contactService = contactService;
     }
--- src/main/java/com/hyperskill/Contacts/services/ContactService.java
+++ src/main/java/com/hyperskill/Contacts/services/ContactService.java
@@ -1,9 +1,11 @@
 package com.hyperskill.Contacts.services;
 
 import com.hyperskill.Contacts.models.Contact;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ContactService {
 
     public List<Contact> getContacts() {
